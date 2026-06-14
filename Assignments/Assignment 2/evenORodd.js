a8a606f82f80b417e2bf611de4cd3bea7d29c47f
Karan Bhatt <140764983+Bhatt2005@users.noreply.github.com>
2026-06-14 21:58:57 +0545

    Add Assignment 1 solutions
    JavaScript and HTML solutions for Web Development Assignment 1.
    Includes even/odd checker, multiplication table, FizzBuzz, 
    square pattern, right-angled triangle pattern, and a dynamic 
    color changer webpage.

---
+++ Assignments/Assignment 2/evenORodd.js
@@ -0,0 +1,10 @@
+function evenOrOdd(number) {
+  if (number % 2 === 0) {
+    console.log(number + " is an even number.");
+  } else {
+    console.log(number + " is an odd number.");
+  }
+}
+
+evenOrOdd(7);
+evenOrOdd(8);
